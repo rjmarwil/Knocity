bb44fd0b5d8c80927bbb027f953af7a6f0dd8ef6
aprout <amprout@gmail.com>
2015-11-23 13:33:28 -0700

    File Structure

--- src/client/main.js
+++ src/client/main.js
@@ -1,9 +1,9 @@
-var myApp = angular.module('myApp', ['ngRoute']);
+var app = angular.module('app', ['ngRoute']);
 
 myApp.config(['$routeProvider', function ($routeProvider) {
   $routeProvider
     .when('/', {
-      templateUrl: 'partials/home.html'
+      templateUrl: 'partials/landing.html'
     })
     .otherwise({redirectTo: '/'});
 }]);
